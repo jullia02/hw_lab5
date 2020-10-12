8c4170a7bc07992bd7943a65c724d6335884374b
Prof. H <profh@cmu.edu>
2020-10-12 16:26:49 -0400

    Giving students a complete parser

--- SwiftRepos/Models/Parser.swift
+++ SwiftRepos/Models/Parser.swift
@@ -2,21 +2,14 @@ import Foundation
 import Alamofire
 
 class Parser {
-  
-  var repositories: [Repository] = []
-  
-  required init() {
-    AF.request("https://api.github.com/search/repositories?q=language:swift&sort=stars&order=desc").responseDecodable(of: Repositories.self) { response in
-      if let value: Repositories = response.value {
-        self.repositories = value.items
-      }
+
+  let urlString = "https://api.github.com/search/repositories?q=language:swift&sort=stars&order=desc"
+
+  func fetchRepositories(completionHandler: @escaping ([Repository]) -> Void) {
+    AF.request(self.urlString).responseDecodable(of: Repositories.self) { (response) in
+      guard let repositories: Repositories = response.value else { return }
+      completionHandler(repositories.items)
     }
   }
-  
-  func update() -> [Repository] {
-    if repositories.isEmpty {
-      _ = type(of: self).init()
-    }
-    return self.repositories
-  }
+
 }
