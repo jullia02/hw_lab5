a879128ac17ee48d7d88fe25444e0ef068ff5d63
Larry Heimann (Prof. H) <profh@cmu.edu>
2023-10-01 20:11:54 -0400

    Update Parser.swift

--- SwiftRepos/Models/Parser.swift
+++ SwiftRepos/Models/Parser.swift
@@ -5,11 +5,18 @@ class Parser {
 
   let urlString = "https://api.github.com/search/repositories?q=language:swift&sort=stars&order=desc"
 
-  func fetchRepositories(completionHandler: @escaping ([Repository]) -> Void) {
-    AF.request(self.urlString).responseDecodable(of: Repositories.self) { (response) in
-      guard let repositories: Repositories = response.value else { return }
-      completionHandler(repositories.items)
-    }
-  }
+  // Old way of doing it with completion handlers...
+  //  func fetchRepositories(completionHandler: @escaping ([Repository]) -> Void) {
+  //    AF.request(self.urlString).responseDecodable(of: Repositories.self) { (response) in
+  //      guard let repositories: Repositories = response.value else { return }
+  //      completionHandler(repositories.items)
+  //    }
+ //  }
+
+   func fetchRepositories() async -> [Repository] {
+     let response = AF.request(self.urlString)
+     let repositories = try? await response.serializingDecodable(Repositories.self).value
+     return repositories?.items ?? []
+   }
 
 }
