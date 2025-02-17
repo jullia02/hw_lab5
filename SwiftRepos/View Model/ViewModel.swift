28b093bfe5cc119faa20caf51d6c2543eef61eeb
jullia02 <jmontejo@andrew.cmu.edu>
2025-02-17 16:04:38 +0300

    added repository view file

--- SwiftRepos/View Model/ViewModel.swift
+++ SwiftRepos/View Model/ViewModel.swift
@@ -18,3 +18,4 @@ class ViewModel: ObservableObject {
 
   }
 }
+
--- SwiftRepos/Views/RepositoryRow.swift
+++ SwiftRepos/Views/RepositoryRow.swift
@@ -1,8 +1,14 @@
 import SwiftUI
 
 struct RepositoryRow: View {
+    var repository: Repository
     var body: some View {
-      // display a repository's name and decription instead of generic Hello World! text
-      Text("Hello World!")
-  }
+        VStack(alignment: .leading){
+            Text(repository.name)
+                .font(.headline)
+            Text(repository.itemDescription ?? "N/A")
+                .font(.subheadline)
+                .lineLimit(1)
+        }
+    }
 }
