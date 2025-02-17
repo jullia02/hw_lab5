c75ad8a0f1b6a4057d968e3aa2ddd745632f5cfa
jullia02 <jmontejo@andrew.cmu.edu>
2025-02-17 21:48:40 +0300

    contentview file

--- SwiftRepos/Views/ContentView.swift
+++ SwiftRepos/Views/ContentView.swift
@@ -1,21 +1,51 @@
 import SwiftUI
 
 struct ContentView: View {
-  
-  // @ObservedObject instance of ViewModel
-  
-  // @State var searchField
-  // @State var displayedRepos
+    
+    @ObservedObject var viewModel = ViewModel()
+    @State var searchField = ""
+    @State var displayedRepos = [Repository]()
   
   var body: some View {
+      let binding = Binding<String>(get: {
+          self.searchField
+      }, set: {
+          self.searchField = $0
+          self.viewModel.search(searchText: self.searchField)
+          self.displayRepos()
+      })
     
-    Text("Hello World!")
-    // replace the generic text above with a VStack as instructed
-    
+      return NavigationView{
+          VStack{
+              TextField("Search", text: binding)
+                  .padding(.leading)
+                  .padding(.top,5)
+              List(displayedRepos) { repository in
+                  NavigationLink(destination: WebView(request: URLRequest(url: URL(string: repository.htmlURL)!))) {
+                      RepositoryRow(repository: repository)
+                  }
+              }
+              .navigationBarTitle("SwiftRepos", displayMode: .inline)
+              Spacer()
+          }.onAppear{
+              Task {
+                  await loadData()
+              }
+          }
+      }
   }
-  
+    func loadData() async{
+        let repos = await Parser().fetchRepositories()
+        self.viewModel.repos = repos
+        self.displayedRepos = repos
+    }
   func displayRepos() {
     // set displayedRepos appropriately
+      if searchField == "" {
+          displayedRepos = viewModel.repos
+      } else {
+          displayedRepos = viewModel.filteredRepos
+      }
   }
 }
 
